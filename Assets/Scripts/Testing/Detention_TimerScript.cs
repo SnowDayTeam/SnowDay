9e03c30a63fe1a999db0a44352535e9101994229
GhostMajik <colin.pugh@georgebrown.ca>
2018-11-14 17:29:16 -0500

    Added new Detention Scene for whitebox programming. Created basic gameplay scripts for detention game mode and implemented a simple jail model for storing the captured students.

---
+++ Assets/Scripts/Testing/Detention_TimerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Detention_TimerScript : MonoBehaviour {
+
+    [SerializeField] private Text timerText;
+    [Range(10, 180)]
+    [SerializeField] public float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = 180;
+        timerText = gameObject.GetComponent<Text>();
+        timerText.text = "Timer: " + timer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        timerText.text = "Timer: " + timer.ToString("f0");
+        if (timer <= 0) {
+            timer = 0;
+            timerText.color = Color.blue;
+            timerText.text = "Kids Win!";
+        }
+	}
+
+    public void SetTimerText(string s, Color c) {
+        timerText.text = s;
+        timerText.color = c;
+        Debug.Log(timerText.text);
+    }
+}
