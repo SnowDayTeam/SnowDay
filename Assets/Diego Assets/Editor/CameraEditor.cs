56d00df891e0868770ad57b0c351dd7423325447
Diego Camacho <DiegoDC97@gmail.com>
2018-11-19 20:34:25 -0500

    Implemented Camera Follow
    Added:
    Scripts:
    -Snow Day Camera.cs
    -CameraEditor.cs
    
    Scene:
    SnowFightUpdated
    
    To Use:
    Modify Paramater in Camera Gimble Game Object:
    Available Parameters:
    
    Player : List<Transform> | List of Players to Track
    Rotation Resolution :  Int  | Number of Points the camera will be able to rotate to
    Rotation Step:  Int  | Current Rotation point camera is at.
    SmoothTime : float | Time it will take the camera to get to its new point
    XZLimiter : float | How far  the camera will go out from the average position between players on the XZ plane
    YLimiter : float | How far  the camera will go up from the average position between players on the Y plane
    EdgeBorderBuffer : float | Small Buffer to allow players to remain in camera view.

---
+++ Assets/Diego Assets/Editor/CameraEditor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof(SnowDayCamera))]
+public class CameraEditor : Editor {
+
+    SerializedProperty PlayerList;
+
+    private void OnEnable()
+    {
+        PlayerList = serializedObject.FindProperty("Players");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(PlayerList, true);
+
+        SnowDayCamera camera = (SnowDayCamera)target;
+
+        EditorGUILayout.LabelField("Rotation Resolution");
+        camera.Resolution = EditorGUILayout.IntSlider(camera.Resolution, 0, 45);
+
+        EditorGUILayout.LabelField("Rotation Step");
+        camera.CurrentStep = EditorGUILayout.IntSlider(camera.CurrentStep, 0, camera.Resolution);
+
+        camera.SmoothTime = EditorGUILayout.FloatField("Smooth Time", camera.SmoothTime);
+
+        camera.XZLimister = EditorGUILayout.FloatField("XZ Limiter", camera.XZLimister);
+
+        camera.YLimiter = EditorGUILayout.FloatField("Y Limiter", camera.YLimiter);
+
+        camera.EdgeBorderBuffer = EditorGUILayout.FloatField("Edge Border Buffer", camera.EdgeBorderBuffer);
+
+
+
+
+        
+
+
+
+
+
+
+        
+
+    }
+}
---
+++ Assets/SnowDayCamera.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowDayCamera : MonoBehaviour
+{
+    [Header("Players")]
+    public List<Transform> Players;
+
+    private Camera cam;
+
+    [Header("Camera Horizontal Rotation")]
+    [Range(0, 10)]
+    public int CurrentStep = 1;
+
+    public float SmoothTime = .5f;
+
+    public float CameraYOffset = 10;
+    public float Radius = 10;
+    public int Resolution = 10;
+
+    public float EdgeBorderBuffer = 5;
+
+    public float XZLimister;
+    public float YLimiter;
+
+    private float thetaStep;
+    private Vector3 camVelocity;
+
+    private Vector3 nextCameraPos;
+
+    // Use this for initialization
+    private void Start()
+    {
+        cam = Camera.main;
+        thetaStep = (2f * Mathf.PI) / Resolution;
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        MoveCamera();
+        CameraDistance();
+    }
+
+    private void CameraDistance()
+    {
+        float greatestDistance = 0;
+        for (int i = 0; i < Players.Count; i++)
+        {
+            if (!Players[i].gameObject.activeSelf)
+                continue;
+
+            float distanceBetween = Vector3.Distance(transform.position, Players[i].position);
+            if (distanceBetween > greatestDistance)
+            {
+                greatestDistance = distanceBetween;
+            }
+        }
+
+        Radius = greatestDistance  + EdgeBorderBuffer / XZLimister;
+        CameraYOffset = greatestDistance  + EdgeBorderBuffer / YLimiter;
+        
+    }
+
+    private void MoveCamera()
+    {
+        transform.position = FindAveragePosition();
+        nextCameraPos = transform.position + new Vector3(Radius * Mathf.Cos(thetaStep * CurrentStep), CameraYOffset, Radius * Mathf.Sin(thetaStep * CurrentStep));//Radius * Mathf.Sin(thetaStep * CurrentStep));
+        cam.transform.position = Vector3.SmoothDamp(cam.transform.position, nextCameraPos, ref camVelocity, SmoothTime);
+        cam.transform.LookAt(transform.position, Vector3.up);
+    }
+
+    private Vector3 FindAveragePosition()
+    {
+        Vector3 averagePosition = new Vector3();
+        int numofPlayers = 0;
+        for (int i = 0; i < Players.Count; i++)
+        {
+            if (!Players[i].gameObject.activeSelf)
+                continue;
+
+            averagePosition += Players[i].position;
+            numofPlayers++;
+        }
+        averagePosition = averagePosition / numofPlayers;
+        averagePosition.y = transform.position.y;
+        return averagePosition;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Vector3 AveragePosition = FindAveragePosition();
+        Gizmos.DrawWireSphere(AveragePosition, 1);
+        Gizmos.color = Color.red;
+        float theta = 0;
+        float thetaStep = (2f * Mathf.PI) / Resolution;
+
+        for (int i = 0; i < Resolution; i++)
+        {
+            Vector3 pos = transform.position + new Vector3(Radius * Mathf.Cos(theta), CameraYOffset, Radius * Mathf.Sin(theta));
+            Gizmos.DrawWireSphere(pos, 1);
+            theta += thetaStep;
+        }
+    }
+}
