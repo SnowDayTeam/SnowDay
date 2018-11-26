a56e5faea433f7d4718e37ffe04af3c97abc3430
NikolaKordic <39223710+NikolaKordic@users.noreply.github.com>
2018-11-25 20:36:06 -0500

    Toboggan controller update
    added camera transformation (press space to look backwards)
    left/right arrow keys swapped once in reverse looking view

--- Assets/Scenes/Toboggan/Assets/Scripts/Controller.cs
+++ Assets/Scenes/Toboggan/Assets/Scripts/Controller.cs
@@ -9,12 +9,16 @@ public class Controller : MonoBehaviour
     public float thrust = 1;
     //sideways force (steering) multiplied by 100 in keyDown
     public float steerThrust = 4;
+    //steering direction based on camera rotation.  1=forwards, -1=negate steering
+    private float steerDir = 1;
 
     public Rigidbody rb;
     public Text speedText;
     public float maxSpeed = 25;
 
     float distFromGround = 0.75f;
+
+    public Camera playerCam;
 
 
     // Use this for initialization
@@ -26,28 +30,43 @@ public class Controller : MonoBehaviour
     void Update()
     {
         transform.localRotation = Quaternion.Euler(0, 0, 0);
-        speedText.text = rb.velocity.z.ToString(); 
+        speedText.text = rb.velocity.z.ToString();
+
 
 
         //increase speed if under max
-        if (rb.velocity.z < maxSpeed) 
-            rb.AddRelativeForce(0, 0, thrust*50, ForceMode.Force);
-        else if (rb.velocity.z > maxSpeed + 1) 
-            rb.AddRelativeForce(0, 0, -thrust*25, ForceMode.Force);
+        if (rb.velocity.z < maxSpeed)
+            rb.AddRelativeForce(0, 0, thrust * 50, ForceMode.Force);
+        else if (rb.velocity.z > maxSpeed + 1)
+            rb.AddRelativeForce(0, 0, -thrust * 25, ForceMode.Force);
         //decrease speed if more than 1 above max
 
         //only add steering input if grounded
-        if (Input.GetKey(KeyCode.LeftArrow) && IsGrounded()) 
+        if (Input.GetKey(KeyCode.LeftArrow) && IsGrounded())
         {
-            rb.AddRelativeForce(-steerThrust*100, 0, 0, ForceMode.Force);
+            rb.AddRelativeForce(-steerThrust * 100 * steerDir, 0, 0, ForceMode.Force);
         }
         if (Input.GetKey(KeyCode.RightArrow) && IsGrounded())
         {
-            rb.AddRelativeForce(steerThrust*100, 0, 0, ForceMode.Force);
+            rb.AddRelativeForce(steerThrust * 100 * steerDir, 0, 0, ForceMode.Force);
         }
-        if(!IsGrounded())
-            Debug.Log("Off Ground"); 
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerCam.transform.rotation = Quaternion.Euler(27, 180, 0); 
+            playerCam.transform.localPosition = new Vector3(0, 3.8f, 1.9f);
+            steerDir = -1;
         }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            playerCam.transform.rotation = Quaternion.Euler(30, 0, 0);
+            playerCam.transform.localPosition = new Vector3(0, 3.8f, -1.9f);
+            steerDir = 1; 
+        }
+
+        if (!IsGrounded())
+            Debug.Log("Off Ground");
+    }
 
     bool IsGrounded()
     {
