a28362a5f1ec9dcc19cde8474359cdfc6d49ce2d
Leron Bergelson <leronbergelson@gmail.com>
2018-12-03 18:13:05 -0500

    Snow Shader Added
    - Snow Shader Implemented.
    - Collision with character feet and snow results in snow tracks.
    - Snow regenerates after a few seconds due to constant snowfall.
    - Particle System implementation for snow.

---
+++ Assets/Scenes/Test Scenes/Shovel Hold Test Scene/SnowNoise.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowNoise : MonoBehaviour {
+
+    public Shader _snowFallShader;
+    private Material _snowFallMat;
+    private MeshRenderer _meshRenderer;
+    [Range(0.001f, 0.1f)]
+    public float _flakeAmount;
+    [Range(0f, 0.1f)]
+    public float _flakeOpacity;
+
+    // Use this for initialization
+    void Start () {
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _snowFallMat = new Material(_snowFallShader);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _snowFallMat.SetFloat("_FlakeAmount", _flakeAmount);
+        _snowFallMat.SetFloat("_FlakeOpacity", _flakeOpacity);
+        RenderTexture snow = (RenderTexture)_meshRenderer.material.GetTexture("_Splat");
+        RenderTexture temp = RenderTexture.GetTemporary(snow.width, snow.height, 0, RenderTextureFormat.ARGBFloat);
+        Graphics.Blit(snow, temp, _snowFallMat);
+        Graphics.Blit(temp, snow);
+        _meshRenderer.material.SetTexture("_Splat", snow);
+        RenderTexture.ReleaseTemporary(temp);
+
+
+    }
+}
