b5acb738aff4836936423414d033f0c5d371616e
OverG0d <ardainan196@gmail.com>
2018-12-18 19:34:54 -0500

    test

---
+++ Assets/Scripts/_Arda/Car.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float carSpeed;
+    //makes the car drive towards the camera
+    public bool driveTowards;
+
+    //handles when the car drives.
+    public float currentDelay;
+    public float minDelay;
+    public float maxDelay;
+
+    private float prevTime;
+    private float currentTime;
+
+    private bool newDelay = true;
+    [HideInInspector]
+    public bool driveMode = false;
+
+    public GameObject LeftLight;
+    public GameObject RightLight;
+
+
+    //testing variables
+    public float LaunchY;
+    public float LaunchX;
+
+
+
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        //handles the various states of the care
+        if (driveMode == true)
+        {
+            drive();
+        }
+
+        //if drive mode isn't active set up the delay then track it
+        else
+        {
+            if (newDelay == true)
+            {
+                SetUpSpawnDelay();
+            }
+
+            else
+            {
+                CheckForDriveTime();
+            }
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    //whatever object in the trigger gets launched to the left or right so they don't get stuck underneath
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (driveTowards == false)
+        {
+            if (rb != null)
+            {
+                rb.AddForce(new Vector3(LaunchX, LaunchY, 0));
+
+            }
+        }
+
+        else
+        {
+            if (rb != null)
+            {
+                rb.AddForce(new Vector3(LaunchX * -1, LaunchY, 0));
+
+            }
+        }
+
+    }
+
+
+    //makes the car drive either toward the camera or away
+    public void drive()
+    {
+        LeftLight.gameObject.SetActive(true);
+        RightLight.gameObject.SetActive(true);
+
+        if (driveTowards == false)
+        {
+            rb.AddForce(new Vector3(carSpeed, 0, 0));
+        }
+
+        else
+        {
+            rb.AddForce(new Vector3((carSpeed) * -1, 0, 0));
+        }
+    }
+
+
+
+    //gets a random amount of time till the next car
+    public void SetUpSpawnDelay()
+    {
+        LeftLight.gameObject.SetActive(false);
+        RightLight.gameObject.SetActive(false);
+        currentDelay = Random.Range(minDelay, maxDelay);
+        prevTime = Time.time;
+        newDelay = false;
+
+
+    }
+
+    //checks to see if delay time is up
+    public void CheckForDriveTime()
+    {
+
+        if (Time.time - prevTime >= currentDelay)
+        {
+            driveMode = true;
+            newDelay = true;
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/_Arda/SendBackCar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendBackCar : MonoBehaviour {
+
+    public bool MoveingTowards;
+    public GameObject awayStart;
+    public GameObject towardStart;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //makes the car immobile
+        if (other.tag == "Car")
+        {
+            if(other.GetComponent<CarDriving>() != null)
+               other.GetComponent<CarDriving>().driveMode = false;
+            if (other.GetComponent<Car>() != null)
+                other.GetComponent<Car>().driveMode = false;
+            other.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+
+
+            //sets the car back on its starting position based on the lane
+            if (MoveingTowards == false)
+            {
+                other.transform.position = awayStart.transform.position;
+            }
+
+            else
+            {
+                other.transform.position = towardStart.transform.position;
+            }
+        }
+    }
+
+}
