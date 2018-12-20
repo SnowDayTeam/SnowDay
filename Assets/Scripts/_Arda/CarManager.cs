593a6c69373edee196257153f9e97da9e1f0cc73
OverG0d <ardainan196@gmail.com>
2018-12-20 18:26:20 -0500

    Worked a bit on car coding

---
+++ Assets/Scripts/_Arda/CarManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarManager : MonoBehaviour {
+
+    public List<GameObject> cars = new List<GameObject>();
+    public int i;
+    public float timer;
+    public float currentDelay = 4;
+
+    public float minDelay;
+    public float maxDelay;
+
+    // Use this for initialization
+    void Start () {
+
+    
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer >= currentDelay)
+        {
+            timer = 0;
+        }
+        
+          
+        
+
+
+	}
+
+    
+}
