8553bc93d536951384e8f6adfa57a6fb11f6a544
Diego Eduardo Camacho <DiegoDC97@gmail.com>
2019-01-24 16:54:36 -0500

    Fixed Camera/Earthquake Bug

---
+++ Assets/Scripts/_Diego/Script/Camera/EarthQuakePower1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EarthQuakePower1 : MonoBehaviour {
+
+    public SnowDayCamera cameraScript;
+    [Range(0.01f, 1)] public float duration;
+    [Range(0.01f, 1)] public float magnitude;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+            //FindObjectOfType<AudioManager>().Play("quake");
+            StartCoroutine(cameraScript.CameraShake(duration, magnitude));
+        }
+    }
+}
