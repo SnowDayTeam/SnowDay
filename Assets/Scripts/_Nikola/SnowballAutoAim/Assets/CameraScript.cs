f14a1ee0a949e7941578c8e839ac89d1c00d30d3
NikolaKordic <39223710+NikolaKordic@users.noreply.github.com>
2019-02-06 09:21:46 -0500

    Snowball AutoAim
    Autoaim script updated.  now has public variable for autoaim range (angle).

--- Assets/Scripts/_Nikola/SnowballAutoAim/Assets/CameraScript.cs
+++ Assets/Scripts/_Nikola/SnowballAutoAim/Assets/CameraScript.cs
@@ -4,20 +4,24 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
 
-    public float snowballDistance = 2f;
-    public float ballThrowForce = 5f;
+    //public float snowballDistance = 2f;
+    //public float ballThrowForce = 5f;
     public GameObject projectile;
 
     public Vector3 cameraRelative;
     private Transform cam;
     private Vector3 relative;
 
-    
+    public float autoAimAngle = 5;
+    GameObject[] gameObjects;
 
     // Use this for initialization
     void Start () {
-        cam = Camera.main.transform; 
-         Vector3 cameraRelative = cam.InverseTransformPoint(transform.position); 
+        //cam = Camera.main.transform; 
+
+        gameObjects = FindObjectsOfType<GameObject>();
+        Debug.Log(gameObjects);
+
 
     }
     public float speedH = 2.0f;
@@ -35,14 +39,43 @@ public class CameraScript : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0))
         {
-            //if (cameraRelative.z > 0)
-            //{
-                GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 500); 
-                 Destroy(bullet, 3);
-           // }
 
+            /*
+            foreach(GameObject eachGameObject in GameObject.      //  .FindGameObjectsWithTag("player"))
+            {
+                if(allObjects.name.Contains(""))
+            }*/
+
+            //get reference to all other objects in the scene - place into array
+            var cube = GameObject.Find("TestCube");
+            Debug.Log(cube); 
+             var pos = cube.transform.position;
+            Debug.Log(pos);
+
+            //get angle between objects
+            float angle = Vector3.Angle(transform.forward, cube.transform.forward); 
+             //Debug.Log("angle: " + angle);
+
+            //Vector3 relativePoint = transform.InverseTransformPoint(cube.transform.position);
+            //Debug.Log("relative point: "+relativePoint);
+
+            //dir'n vector player to target.
+            Vector3 directionToTarget = cube.transform.position - this.transform.position;
+
+
+            GameObject bullet;
+            bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
             
+            //if (relativePoint.x > -1 && relativePoint.x < 1 && relativePoint.y > -1 && relativePoint.y < 1)
+            if (angle < autoAimAngle)
+            {
+                bullet.GetComponent<Rigidbody>().AddForce(directionToTarget * 100); 
+            }
+            else
+            {
+                bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 500); 
+            }
+            Destroy(bullet, 3); 
         }
 
     }
