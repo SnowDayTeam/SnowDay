e5561dde10254dbe402d0d555f8eb79898ac66cc
SJH607-11W\Graphic <carlospidla@gmail.com>
2019-03-08 17:45:47 -0500

    shovel setup

---
+++ Assets/ShovelLerpController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShovelLerpController : MonoBehaviour {
+
+    public lerpPosition drawbackPos;
+    public lerpPosition groundPos;
+    public lerpPosition throwPos;
+    public lerpPosition neutralPos;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
