90a0609ff51b6f7b4ff649989aaa6368ecd9b2a4
cspidla <carlospidla@gmail.com>
2019-03-31 23:42:46 -0400

    loading screens

---
+++ Assets/CarGateTrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarGateTrigger : MonoBehaviour
+{
+
+    bool up;
+    public float speed;
+    public GameObject gate;
+    Animator animator;
+
+    void Start()
+    {
+        animator = gate.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (up == true)
+        {
+            animator.SetTrigger("Open");
+        }
+        else
+        {
+            animator.SetTrigger("Close");
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+        {
+            up = true;
+        }
+
+    }
+
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Car")
+        {
+            up = false;
+        }
+
+    }
+}
