883d651904536142347dfded71ce069f804fe5f7
SJH607-11W\Graphic <carlospidla@gmail.com>
2019-04-12 20:11:54 -0400

    smaller levels

--- Assets/ProjectorRotation.cs
+++ Assets/ProjectorRotation.cs
@@ -6,6 +6,7 @@ public class ProjectorRotation : MonoBehaviour {
 
     Vector3 camPos;
     float thisHeight;
+    float timer = 10;
 	// Use this for initialization
 	void Start () {
         camPos = Camera.main.transform.position;
@@ -15,6 +16,13 @@ public class ProjectorRotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.LookAt(camPos);
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            camPos = Camera.main.transform.position;
+            camPos.y = transform.position.y;
+            timer = 10;
+        }
     }
 }
 //var targetObj: Transform;
